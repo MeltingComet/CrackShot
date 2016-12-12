124a9527d80455649e76914218c911a472e8bb4f
JokieW <gulliverjko@gmail.com>
2016-12-12 01:47:43 -0500

    - Fixed batch files to not need a steam path - Fixed restoreSteam not cleaning the folders properly

--- b_unpack.bat
+++ b_unpack.bat
@@ -3,33 +3,32 @@
 SET modified=%CD%\Modified
 SET local=%CD%
 
+if not exist "%modified%\" goto:eof
 
 echo.Merging files from %modified%
-if exist "%modified%\" (
-	CALL :dofolder "\Audio\BGM\"
-	CALL :dofolder "\Audio\BGS\"
-	CALL :dofolder "\Audio\ME\"
-	CALL :dofolder "\Audio\SE\"
-	CALL :dofolder "\Data\"
-	CALL :dofolder "\Fonts\"
-	CALL :dofolder "\Graphics\Animations\"
-	CALL :dofolder "\Graphics\Autotiles\"
-	CALL :dofolder "\Graphics\Characters\"
-	CALL :dofolder "\Graphics\Faces\"
-	CALL :dofolder "\Graphics\Fogs\"
-	CALL :dofolder "\Graphics\Icons\"
-	CALL :dofolder "\Graphics\Lightmaps\"
-	CALL :dofolder "\Graphics\Lights\"
-	CALL :dofolder "\Graphics\Menus\"
-	CALL :dofolder "\Graphics\Misc\"
-	CALL :dofolder "\Graphics\Panoramas\"
-	CALL :dofolder "\Graphics\Pictures\"
-	CALL :dofolder "\Graphics\Tilesets\"
-	CALL :dofolder "\Graphics\Titles\"
-	CALL :dofolder "\Graphics\Transitions\"
-	CALL :dofolder "\Graphics\Windowskins\"
-	CALL :dofolder "\Wallpaper\"
-)
+CALL :dofolder "\Audio\BGM\"
+CALL :dofolder "\Audio\BGS\"
+CALL :dofolder "\Audio\ME\"
+CALL :dofolder "\Audio\SE\"
+CALL :dofolder "\Data\"
+CALL :dofolder "\Fonts\"
+CALL :dofolder "\Graphics\Animations\"
+CALL :dofolder "\Graphics\Autotiles\"
+CALL :dofolder "\Graphics\Characters\"
+CALL :dofolder "\Graphics\Faces\"
+CALL :dofolder "\Graphics\Fogs\"
+CALL :dofolder "\Graphics\Icons\"
+CALL :dofolder "\Graphics\Lightmaps\"
+CALL :dofolder "\Graphics\Lights\"
+CALL :dofolder "\Graphics\Menus\"
+CALL :dofolder "\Graphics\Misc\"
+CALL :dofolder "\Graphics\Panoramas\"
+CALL :dofolder "\Graphics\Pictures\"
+CALL :dofolder "\Graphics\Tilesets\"
+CALL :dofolder "\Graphics\Titles\"
+CALL :dofolder "\Graphics\Transitions\"
+CALL :dofolder "\Graphics\Windowskins\"
+CALL :dofolder "\Wallpaper\"
 goto:eof
 
 :dofolder
